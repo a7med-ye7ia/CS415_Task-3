3c239ad5b67eba0107f796a20cf173a930de5dec
a7med-ye7ia <anaa76746@gmail.com>
2025-10-17 22:13:38 +0300

    modify folder structure

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,8 +1,7 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.assertEquals;
-
-public class CalculatorTesting {
+public class CalculatorTest {
 
     @Test
     public void testAdd(){
